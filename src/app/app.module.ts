38388c13835bf42eb3e8b24731a73264bd6bcadc
JenniferKuo <meappp118@gmail.com>
2018-01-08 01:12:29 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { IntroPage } from '../pages/intro/intro';
+import { MapPage } from '../pages/map/map';
+import { LinksPage } from '../pages/links/links';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,22 +14,25 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    IntroPage,
+    MapPage,
+    LinksPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    IntroPage,
+    MapPage,
+    LinksPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
