877a948f8f469da1bf1b9dbd4a30abd3082f9d3c
JenniferKuo <meappp118@gmail.com>
2018-01-26 17:52:41 +0800

    link to json in link.ts file

--- src/pages/links/links.ts
+++ src/pages/links/links.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-links',
@@ -7,7 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class LinksPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: Http, public navCtrl: NavController) {
   }
   
+  ionViewDidLoad() {
+  	this.loadContent();
+  }
+
+  loadContent() {
+  	this.http.get('https://kaohsiungmemory.com/json/console_setting.json')
+  		.map(res => res.json())
+  		.subscribe(data => {
+  			this.data = data.links.content;
+  			console.log(this.data);
+  		},err => {
+  			console.log(err);
+  		});
+  }
 }
